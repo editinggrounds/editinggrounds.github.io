af816b204ee49003dd99a89e723dece2828a6d4d
editinggrounds <168940671+editinggrounds@users.noreply.github.com>
2024-05-05 09:52:37 +1000

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 3000 });
+
+wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(message) {
+        wss.clients.forEach(function each(client) {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+});
